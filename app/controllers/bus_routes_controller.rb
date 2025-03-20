f6965a6e345f7822284fffe31fdd72a386f18077
tpatel37 <tpatel37@rrc.ca>
2025-03-19 21:45:39 -0500

    Added collection Navigation functionality

--- app/controllers/bus_routes_controller.rb
+++ app/controllers/bus_routes_controller.rb
@@ -2,4 +2,8 @@ class BusRoutesController < ApplicationController
   def index
     @bus_routes = BusRoute.all
   end
+  def show
+    @bus_route = BusRoute.find(params[:id])
+  end
+  
 end
--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,4 +2,8 @@ class StopsController < ApplicationController
   def index
     @stops = Stop.all
   end
+  def show
+    @stop = Stop.find(params[:id])
+  end
+  
 end
